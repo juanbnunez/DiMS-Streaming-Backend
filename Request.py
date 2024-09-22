45d3d4b1195da789b05cb45048d159451c02543c
JENNIFER GONZALEZ SOLIS <gonzalezsolisjennifer@gmail.com>
2024-09-22 13:46:05 -0600

    vercel connection

--- Request.py
+++ Request.py
@@ -2,39 +2,32 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from Content import Content
 
-class Request:
-    def __init__(self):
-        self.app = Flask(__name__)
-        CORS(self.app)  # Permitir CORS para todas las rutas
-        self.content_manager = Content()
-        self.setup_routes()
+app = Flask(__name__)
+CORS(app)  # Permitir CORS para todas las rutas
 
-    def setup_routes(self):
-        @self.app.route('/get-media', methods=['GET'])
-        def get_media():
-            """
-            Endpoint para obtener los archivos agrupados de S3.
-            Retorna un JSON con el nombre, la URL de la imagen y la URL de audio o video.
-            """
-            try:
-                # Obtener los archivos agrupados por nombre base
-                grouped_files = self.content_manager.group_s3_objects_by_name()
+content_manager = Content()
 
-                # Convertir los datos a un formato más adecuado para el frontend
-                media_data = []
-                for name, files in grouped_files.items():
-                    media_data.append({
-                        'name': name,
-                        'image_url': files.get('image'),
-                        'audio_url': files.get('audio')
-                    })
-                return jsonify(media_data), 200
-            except Exception as e:
-                return jsonify({"error": str(e)}), 500  # Manejo de errores
+@app.route('/get-media', methods=['GET'])
+def get_media():
+    """
+    Endpoint para obtener los archivos agrupados de S3.
+    Retorna un JSON con el nombre, la URL de la imagen y la URL de audio o video.
+    """
+    try:
+        # Obtener los archivos agrupados por nombre base
+        grouped_files = content_manager.group_s3_objects_by_name()
 
-    def run(self):
-        self.app.run(debug=True, host='0.0.0.0', port=5000)  # Iniciar el servidor
+        # Convertir los datos a un formato más adecuado para el frontend
+        media_data = []
+        for name, files in grouped_files.items():
+            media_data.append({
+                'name': name,
+                'image_url': files.get('image'),
+                'audio_url': files.get('audio')
+            })
+        return jsonify(media_data), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500  # Manejo de errores
 
-if __name__ == '__main__':
-    app = Request()
-    app.run()
+# El punto de entrada de Flask es ahora la función app
+
