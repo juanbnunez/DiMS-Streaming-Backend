b539df1968e051b6da9bf5e2db38b5be665d9533
JENNIFER GONZALEZ SOLIS <gonzalezsolisjennifer@gmail.com>
2024-09-22 21:46:52 -0600

    time test

--- Request.py
+++ Request.py
@@ -1,33 +1,47 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from Content import Content
+import time
 
-app = Flask(__name__)
-CORS(app)  # Permitir CORS para todas las rutas
+class Request:
+    def __init__(self):
+        self.app = Flask(__name__)
+        CORS(self.app)  # Permitir CORS para todas las rutas
+        self.content_manager = Content()
+        self.setup_routes()
 
-content_manager = Content()
+    def setup_routes(self):
+        @self.app.route('/get-media', methods=['GET'])
+        def get_media():
+            """
+            Endpoint para obtener los archivos agrupados de S3.
+            Retorna un JSON con el nombre, la URL de la imagen y la URL de audio o video.
+            """
+            try:
+                start_time = time.time()
+                # Obtener los archivos agrupados por nombre base
+                grouped_files = self.content_manager.group_s3_objects_by_name()
 
-@app.route('/get-media', methods=['GET'])
-def get_media():
-    """
-    Endpoint para obtener los archivos agrupados de S3.
-    Retorna un JSON con el nombre, la URL de la imagen y la URL de audio o video.
-    """
-    try:
-        # Obtener los archivos agrupados por nombre base
-        grouped_files = content_manager.group_s3_objects_by_name()
+                # Convertir los datos a un formato más adecuado para el frontend
+                media_data = []
+                for name, files in grouped_files.items():
+                    media_data.append({
+                        'name': name,
+                        'image_url': files.get('image'),
+                        'audio_url': files.get('audio')
+                    })
+                
+                end_time = time.time()  # Detiene el temporizador
+                duration = end_time - start_time
+                print(f"Tiempo de carga en el backend: {duration:.4f} segundos")  # Muestra el informe en consola
 
-        # Convertir los datos a un formato más adecuado para el frontend
-        media_data = []
-        for name, files in grouped_files.items():
-            media_data.append({
-                'name': name,
-                'image_url': files.get('image'),
-                'audio_url': files.get('audio')
-            })
-        return jsonify(media_data), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500  # Manejo de errores
+                return jsonify(media_data), 200
+            except Exception as e:
+                return jsonify({"error": str(e)}), 500  # Manejo de errores
 
-# El punto de entrada de Flask es ahora la función app
+    def run(self):
+        self.app.run(debug=True, host='0.0.0.0', port=5000)  # Iniciar el servidor
 
+if __name__ == '__main__':
+    app = Request()
+    app.run()
